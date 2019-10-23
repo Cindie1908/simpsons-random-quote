e933b9b129cf655d595ab26b96e353fdf497535a
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-10-23 14:11:22 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DisplaySimpsons from './components/DisplaySimpsons';
+//import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*const exSimpson = {
+quote:"Last night's 'Itchy and Scratchy' was, without a doubt, the worst episode ever. Rest assured that I was on the Internet within minutes, registering my disgust throughout the world.",
+character:"Comic Book Guy",
+image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FComicBookGuy.png?1497567511970",
+characterDirection:"Right"
+}*/
+
+class App extends React.Component{
+  constructor (props){
+    super(props)
+    this.state={
+      exSimpson:[]
+    }
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount() {
+    // Send the request
+    fetch('https://quests.wilders.dev/simpsons-quotes/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.json())
+
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          exSimpson: data,
+        });
+    });
+  }
+  render(){
+    const {exSimpson} = this.state
+    return (
+      <div className="App">
+        <DisplaySimpsons exSimpson={exSimpson} />
+        <button type="button" onClick={this.componentDidMount}>Get new quote</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DisplaySimpsons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function DisplaySimpsons({exSimpson}) {
+  return (
+    <div className="DisplaySimpsons">
+      <img
+        src={exSimpson[0] && exSimpson[0].image}
+        alt={exSimpson[0] && exSimpson[0].character}
+      />
+      <ul>
+        <li>{exSimpson[0] && exSimpson[0].character}</li>
+        <li>{exSimpson[0] && exSimpson[0].quote}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DisplaySimpsons;
